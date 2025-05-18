fdccc6745b2a23dff8261e98d7cda7463d19e231
Hamza HN <143803507+HAMZA25032005@users.noreply.github.com>
2025-05-18 16:22:13 +0100

    Delete Livrable 2/project directory

--- Livrable 2/project/Utils/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Utils
-{
-    public static class AppSettings
-    {
-        // Valeur par défaut : "JSON" ou "XML"
-        public static string LogFormat { get; set; } = "JSON";
-    }
-}
